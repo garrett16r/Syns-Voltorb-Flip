2dbbb5629dc3079db2e2a5d1010701fd294b5b90
Syntthetix <garrett513r@gmail.com>
2019-02-28 00:21:42 -0500

    Added more basic functionality
    + Labels displaying total points on each row/col
    - Need to add labels displaying total Voltorbs on each row/col

--- Syns-Voltorb-Flip/Game.cs
+++ Syns-Voltorb-Flip/Game.cs
@@ -8,7 +8,6 @@ namespace Syns_Voltorb_Flip
 {
     public class Game
     {
-
         /* Grid:
          * [] [] [] [] []
          * [] [] [] [] []
@@ -22,19 +21,34 @@ namespace Syns_Voltorb_Flip
         int num3s;
         int turned2s;
         int turned3s;
+        int[] rowPointSums;
+        int[] colPointSums;
         VoltorbFlipForm vff;
+
+        public Game(VoltorbFlipForm vff)
+        {
+            this.vff = vff;
+        }
 
         public void start()
         {
-            vff = new VoltorbFlipForm();
             grid = new int[5, 5];
             score = 0;
             num2s = 0;
             num3s = 0;
             turned2s = 0;
             turned3s = 0;
+            rowPointSums = new int[5] { 0, 0, 0, 0, 0 };
+            colPointSums = new int[5] { 0, 0, 0, 0, 0 };
             generateGrid();
+
             debugPrintGrid();
+        }
+
+        // Resets the board for a new game after winning or losing a game
+        public void resetBoard()
+        {
+
         }
 
         // Creates the 2D array representing the on-screen grid. Each coordinate holds a value representing a 1, 2, 3, or Voltorb.
@@ -47,8 +61,10 @@ namespace Syns_Voltorb_Flip
                 for (int col = 0; col < 5; col++)
                 {
                     int val = rand.Next(4);
-                    grid[row, col] = val; // fills the coordinate with a random int from 0 to 3. 0 = Voltorb
-                    
+                    grid[row, col] = val; // Fills the coordinate with a random int from 0 to 3. 0 = Voltorb
+                    colPointSums[col] += val; // Adds the number on the current tile to the sum of the points on the current column
+                    rowPointSums[row] += grid[row, col]; // Adds the number on the current tile to the sum of the points on the current row
+
                     // Adds up the total number of 2 and 3 tiles, used for checking the win condition.
                     if (val == 2)
                     {
@@ -59,6 +75,7 @@ namespace Syns_Voltorb_Flip
                     }
                 }
             }
+            vff.updatePointSums(rowPointSums, colPointSums);
         }
 
         public int checkTile(int row, int col)
@@ -67,12 +84,13 @@ namespace Syns_Voltorb_Flip
 
             if (tileVal != 0)
             {
-                if (score == 0 && tileVal == 1)
+                if (score == 0)
                 {
-                    score = 1;
+                    score = tileVal;
                 } else
                 {
                     score *= tileVal;
+
                     if (tileVal == 2)
                     {
                         turned2s++;
@@ -101,15 +119,32 @@ namespace Syns_Voltorb_Flip
 
         public void win()
         {
-
+            Console.WriteLine("Win");
         }
 
         public void gameOver()
         {
-
+            Console.WriteLine("Game over");
+           //start();
         }
 
-        // Prints the grid to the console for debugging purposes.
+        // Print the point sums for each row and column for debuggings purposes
+        public void debugPrintRowColSums()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Row " + i + " = " + rowPointSums[i]);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Col " + i + " = " + colPointSums[i]);
+            }
+        }
+
+        // Prints the grid to the console for debugging purposes
         public void debugPrintGrid()
         {
             for (int row = 0; row < 5; row++)
--- Syns-Voltorb-Flip/VoltorbFlip.cs
+++ Syns-Voltorb-Flip/VoltorbFlip.cs
@@ -12,16 +12,60 @@ namespace Syns_Voltorb_Flip
 {
     public partial class VoltorbFlipForm : Form
     {
-        Game game = new Game();
+        Game game;
 
         public VoltorbFlipForm()
         {
             InitializeComponent();
+            game = new Game(this);
         }
 
         private void VoltorbFlipForm_Load(object sender, EventArgs e)
-        {            
+        {
+            setLblColors();
             game.start();
+        }
+
+        public void setLblColors()
+        {
+            voltorbRow0Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbRow1Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbRow2Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbRow3Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbRow4Lbl.BackColor = System.Drawing.Color.Transparent;
+
+            voltorbCol0Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbCol1Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbCol2Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbCol3Lbl.BackColor = System.Drawing.Color.Transparent;
+            voltorbCol4Lbl.BackColor = System.Drawing.Color.Transparent;
+
+            pointSumRow0Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumRow1Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumRow2Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumRow3Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumRow4Lbl.BackColor = System.Drawing.Color.Transparent;
+
+            pointSumCol0Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumCol1Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumCol2Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumCol3Lbl.BackColor = System.Drawing.Color.Transparent;
+            pointSumCol4Lbl.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        public void updatePointSums(int[] rowPointSums, int[] colPointSums)
+        {
+            pointSumRow0Lbl.Text = "Pts: " + rowPointSums[0];
+            pointSumRow1Lbl.Text = "Pts: " + rowPointSums[1];
+            pointSumRow2Lbl.Text = "Pts: " + rowPointSums[2];
+            pointSumRow3Lbl.Text = "Pts: " + rowPointSums[3];
+            pointSumRow4Lbl.Text = "Pts: " + rowPointSums[4];
+
+            pointSumCol0Lbl.Text = "Pts: " + colPointSums[0];
+            pointSumCol1Lbl.Text = "Pts: " + colPointSums[1];
+            pointSumCol2Lbl.Text = "Pts: " + colPointSums[2];
+            pointSumCol3Lbl.Text = "Pts: " + colPointSums[3];
+            pointSumCol4Lbl.Text = "Pts: " + colPointSums[4];
         }
 
         // Uses the name of a tile to obtain its row/col for reference in the grid array.
@@ -38,7 +82,7 @@ namespace Syns_Voltorb_Flip
         {
             Button tile = sender as Button; // Gets the button that was clicked, making this a universal onClick method.
 
-            tile.Enabled = false;
+            //tile.Enabled = false;
             getTileCoord(ref tile);
         }
     }
